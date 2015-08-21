7042e6e630bd92ce5e86e0eb082e3a4754b801e7
Joseph Burnett <josephburnett79@gmail.com>
2015-08-20 22:22:37 -0700

    Stub load test.

---
+++ bin/load_test.py
@@ -0,0 +1,95 @@
+#    Copyright 2009 Google Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+"""Simple web application load testing script.
+
+This is a simple web application load
+testing skeleton script. Modify the code between !!!!!
+to make the requests you want load tested.
+"""
+
+
+import httplib2
+import random
+import socket
+import time
+from threading import Event
+from threading import Thread
+from threading import current_thread
+from urllib import urlencode
+
+# Modify these values to control how the testing is done
+
+# How many threads should be running at peak load.
+NUM_THREADS = 1
+
+# How many minutes the test should run with all threads active.
+TIME_AT_PEAK_QPS = 0.1 # minutes
+
+# How many seconds to wait between starting threads.
+# Shouldn't be set below 30 seconds.
+DELAY_BETWEEN_THREAD_START = 30 # seconds
+
+# Which endpoint should we hit?
+ENDPOINT = "http://localhost:8080"
+
+quitevent = Event()
+
+def staticGame(h):
+    """Play a predetermined game of K-D Tic Tac Toe."""
+    resp, cont = h.request(ENDPOINT)
+    if resp.status != 200:
+        print "Response not OK"
+    if resp['content-location'].startswith(ENDPOINT):
+        print "Unexpected content-location"
+    resp, _ = h.request(ENDPOINT+"/game?handle=Player%201;playerCount=2;k=2;size=3;inarow=3")
+    if resp.status != 200:
+        print "Response not OK"
+    if resp['content-location'].startswith(ENDPOINT+"/game"):
+        print "Unexpected content-location"
+
+def threadproc():
+    """This function is executed by each thread."""
+    print "Thread started: %s" % current_thread().getName()
+    h = httplib2.Http(timeout=30)
+    while not quitevent.is_set():
+        try:
+            staticGame(h)
+        except socket.timeout:
+            pass
+
+    print "Thread finished: %s" % current_thread().getName()
+
+
+if __name__ == "__main__":
+    runtime = (TIME_AT_PEAK_QPS * 60 + DELAY_BETWEEN_THREAD_START * NUM_THREADS)
+    print "Total runtime will be: %d seconds" % runtime
+    threads = []
+    try:
+        for i in range(NUM_THREADS):
+            t = Thread(target=threadproc)
+            t.start()
+            threads.append(t)
+            time.sleep(DELAY_BETWEEN_THREAD_START)
+        print "All threads running"
+        time.sleep(TIME_AT_PEAK_QPS*60)
+        print "Completed full time at peak qps, shutting down threads"
+    except:
+        print "Exception raised, shutting down threads"
+
+    quitevent.set()
+    time.sleep(3)
+    for t in threads:
+        t.join(1.0)
+    print "Finished"
